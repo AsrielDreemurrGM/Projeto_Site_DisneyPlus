526060dd4022b631360470bea1ee9fe2df9b8555
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-09-07 01:23:07 -0300

    Finalizado Cabeçalho
    - Criado Código JavaScript Para Ocultar e Exibir o Cabeçalho Conforme Posição na Página;
    - Organizado Documento JavaScript Com Comentários para Facilitar Entendimento;

--- src/main.js
+++ src/main.js
@@ -3,17 +3,33 @@ document.addEventListener('DOMContentLoaded', function() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    // Controla o Display do Cabeçalho
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosHeader();
+        } else {
+            exibeElementosHeader();
+        }
+    })
+
+    // Troca de Abas - Seção Atrações
     for (let i= 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(button) {
             const targetTab = button.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${targetTab}]`);
+            const tab = document.querySelector(`[data-tab-id=${targetTab}]`);
             hideAllTabs();
-            aba.classList.add('shows__list--is-active')
+            tab.classList.add('shows__list--is-active')
             activeButton();
             button.target.classList.add('shows__tabs__button--is-active');
         })
     }
 
+    // Abrir e Fechar Perguntas do FAQ
     for (let i = 0; i < questions.length;i ++) {
         questions[i].addEventListener('click', openOrCloseAnswer);
     }
@@ -25,6 +41,16 @@ function activeButton() {
     for (let i= 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
+}
+
+function ocultaElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
 
 function hideAllTabs() {
